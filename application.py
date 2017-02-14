a3e8ebc5f185413810be3d53418d7b2d112de995
Robert Colin <cfflymolo@gmail.com>
2017-02-13 22:39:29 -0800

    Defined basic hello world project

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def hello():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
